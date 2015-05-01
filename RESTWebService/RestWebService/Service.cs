ff5416d7618488dcb11c40a0a186baef03bd5776
Bernardo A. Munoz <bernardoamf@hotmail.com>
2015-04-30 22:57:24 -0700

    Implemented get

---
+++ RESTWebService/RestWebService/Service.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Company;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+
+namespace RestWebService
+{
+    public class Service : IHttpHandler
+    {
+        #region private Memebers
+        
+        //private string connString;
+
+        #endregion
+        
+        #region Handler
+        bool IHttpHandler.IsReusable
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        void IHttpHandler.ProcessRequest(HttpContext context)
+        {
+            try
+            {
+                string url = Convert.ToString(context.Request.Url);
+
+                switch (context.Request.HttpMethod)
+                {
+                    case "GET":
+                        READ(context);
+                        break;
+                    case "POST":
+                        CREATE(context);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        #endregion Handler
+
+        #region CRUD Functions
+
+        private void READ(HttpContext context)
+        {
+            //HTTP Request - //http://server.com/virtual directory/employee?id={id}
+            //http://localhost/RestWebService/employee
+
+            int employeeCode = Convert.ToInt16(context.Request["id"]);
+            CompanyLinqDataContextDataContext db = new CompanyLinqDataContextDataContext();
+
+            var employee = from e in db.Employees
+                           where e.id = employeeCode
+                           select e;
+            string serializedEmployee = Serialize(employee);
+            context.Response.ContentType = "text/xml";
+            WriteResponse(serializedEmployee);
+        }
+
+        #endregion CRUD Functions
+
+        #region Utility Functions
+
+        private static void WriteResponse(string strMessage)
+        {
+            HttpContext.Current.Response.Write(strMessage);
+        }
+
+        private String Serialize(Employee emp)
+        {
+            String XmlizedString = null;
+            XmlSerializer xs = new XmlSerializer(typeof(Company.Employee));
+            MemoryStream memoryStream = new MemoryStream();
+            XmlTextWriter xmlTextWriter = new XmlTextWriter(memoryStream, Encoding.UTF8);
+            xs.Serialize(xmlTextWriter, emp);
+            memoryStream = (MemoryStream)xmlTextWriter.BaseStream;
+            XmlizedString = UTF8ByteArrayToString(memoryStream.ToArray());
+            return XmlizedString;
+        }
+
+        private String UTF8ByteArrayToString(Byte[] characters)
+        {
+            UTF8Encoding encoding = new UTF8Encoding();
+            String constructedString = encoding.GetString(characters);
+            return (constructedString);
+        }
+
+        #endregion Utility Functions
+
+
+    }
+}
---
+++ RESTWebService/Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company;
+using System.Data.Linq;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompanyLinqDataContextDataContext db = new CompanyLinqDataContextDataContext();
+
+            var employeeList = from e in db.Employees
+                            select e;
+
+
+            foreach (var e in employeeList)
+            {
+                Console.WriteLine("Name: {0}", e.FirstName);
+                Console.WriteLine("Last Name: {0}", e.LastName);
+            }
+            Console.Read();
+	{
+		 
+	}
+        }
+    }
+}
